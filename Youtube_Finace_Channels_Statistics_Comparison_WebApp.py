e78873f337e4475a15a019d2ce9136ae9d016c24
Arnab Das <70911619+Arnab-28@users.noreply.github.com>
2024-10-25 21:21:35 +0530

    Update Youtube_Finace_Channels_Statistics_Comparison_WebApp.py

--- Youtube_Finace_Channels_Statistics_Comparison_WebApp.py
+++ Youtube_Finace_Channels_Statistics_Comparison_WebApp.py
@@ -98,6 +98,7 @@ def plot_bar_chart_with_values(data, ax, x_col, y_col, title, xlabel):
             ax.bar_label(container, fmt='%.2f', color='white', fontsize=10)
 
     except Exception:
+        return None
         #st.error("Server is busy now! Please try again later.")
 
 # Streamlit app configuration
@@ -122,6 +123,7 @@ if not st.session_state.channel_data.empty:
                                            st.session_state.channel_data['Channel_name'],
                                            default=st.session_state.channel_data['Channel_name'])
 else:
+    return None
     #st.warning("No channel data available. Please try again later.")
 
 # Text input for adding a new channel
